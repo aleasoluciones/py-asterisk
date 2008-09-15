db2762691451e93d8e84e4a907f2051b7d4f50c6
clintonjames <devnull@localhost>
2008-09-15 16:59:06 +0000

    Cleanup

--- setup.py
+++ setup.py
@@ -7,21 +7,22 @@ py-Asterisk distutils script.
 __author__ = 'David M. Wilson <dw-py-Asterisk-setup.py@botanicus.net>'
 __id__ = '$Id$'
 
-from distutils.core import setup
-
-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 
 setup(
     name =          'py-Asterisk',
-	version =       '0.1',
-	description =   'Asterisk Manager API Python interface.',
-	author =        'David M. Wilson',
-	author_email =  'dw-py-Asterisk-setup.py@botanicus.net',
-	license =       'MIT',
-	url =           'http://botanicus.net/dw/',
-	download_url =  'http://botanicus.net/dw/py-asterisk.php',
-	packages =      [ 'Asterisk' ],
-	scripts =       [ 'asterisk-dump', 'py-asterisk' ],
+    version =       '0.1',
+    description =   'Asterisk Manager API Python interface.',
+    author =        'David M. Wilson',
+    author_email =  'dw-py-Asterisk-setup.py@botanicus.net',
+    license =       'MIT',
+    url =           'http://botanicus.net/dw/',
+    download_url =  'http://botanicus.net/dw/py-asterisk.php',
+    packages =      [ 'Asterisk' ],
+    scripts =       [ 'asterisk-dump', 'py-asterisk' ],
     data_files =    [ ('/etc/asterisk', ['conf/py-asterisk.conf.sample']) ]
 )
