5ae5ee660ccb8f9a1a63ef7c44886d2ce8a546be
Raúl Villares <rvillares@alea-soluciones.com>
2019-05-10 08:47:21 +0200

    [INTERNAL] Change Asterisk Call Manager version validation
    - Now, Asterisk Call manager version is valid if starts with prefix
    'Asterisk Call Manager'
    - Inspired in
    [https://github.com/jeansch/py-asterisk/blob/237173ee81a33ce8e9fe6395227c8c8a40563661/Asterisk/Manager.py](jeansch/py-asterisk)

--- Asterisk/Manager.py
+++ Asterisk/Manager.py
@@ -179,14 +179,7 @@ class ZapChannel(BaseChannel):
 class BaseManager(Asterisk.Logging.InstanceLogger):
     'Base protocol implementation for the Asterisk Manager API.'
 
-    _AST_BANNERS = [
-        'Asterisk Call Manager/1.0\r\n',
-        'Asterisk Call Manager/1.1\r\n',
-        'Asterisk Call Manager/1.2\r\n',
-        'Asterisk Call Manager/1.3\r\n',
-        'Asterisk Call Manager/2.8.0\r\n',
-        'Asterisk Call Manager/2.10.4\r\n',
-    ]
+    _AST_BANNER_PREFIX = 'Asterisk Call Manager'
 
 
     def __init__(self, address, username, secret, listen_events=True,
@@ -231,9 +224,9 @@ class BaseManager(Asterisk.Logging.InstanceLogger):
         'Read the server banner and attempt to authenticate.'
 
         banner = self.file.readline()
-        if banner not in self._AST_BANNERS:
+        if not banner.starts_with(self._AST_BANNER_PREFIX):
             raise Exception('banner incorrect; got %r, expected one of %r' %\
-                            (banner, self._AST_BANNERS))
+                            (banner, self._AST_BANNER_PREFIX))
 
         action = {
             'Username': self.username,
